e3a160c9cf0442e59ca7b6972268d7f22c7977e1
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-05-24 15:15:11 +0000

    connect to MongoDb

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+import env
 
+# The first part of set-ups is inspired by the course materials, mini project "Task Manager":
 app = Flask(__name__)
 
+app.config["MONGO_URI"]= "mongodb+srv://root:r00tUser@myfirstcluster-vdori.mongodb.net/speak?retryWrites=true&w=majority"
+MONGODB_URI = os.getenv("MONGO_URI")
+DBS_NAME = "speak"
+COLLECTION_NAME = "categories"
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Helo World"
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
---
+++ env.py
@@ -0,0 +1,2 @@
+import os
+os.environ.setdefault("MONGO_URI", "mongodb+srv://root:r00tUser@myfirstcluster-vdori.mongodb.net/speak?retryWrites=true&w=majority")
